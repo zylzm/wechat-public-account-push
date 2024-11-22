a6661ee0285e2961c921d5f0186482922edb86e8
zylzm <1726265801@qq.com>
2024-11-22 09:57:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oS2Zu6zJaUlkdZoalkd25hDZFF9c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'YKok_9lIDTIXtjXVtTvpWx3Q-Y4QEQT3C3YgBG_e4zk',
+      useTemplateId: '23ZvWlcosseHmt-Em2rrHfm0vqRN58aGj9KbOceGG8g',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-23',
       festivals: [
